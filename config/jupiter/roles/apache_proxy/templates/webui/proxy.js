15aba3d21628c168c85642801ad32fc7b22623bc
Olivian Daniel Tofan <daniel@toffee.ro>
2021-01-24 23:29:31 +0200

    Include cameras into web_ui

--- config/jupiter/roles/apache_proxy/templates/webui/proxy.js
+++ config/jupiter/roles/apache_proxy/templates/webui/proxy.js
@@ -1,6 +1,18 @@
 var html = '<div class="service imageWatcher">';
-html += '<div style="cursor:pointer" onClick="mx.Actions.openEntryById(event,\'automation\',\'cameras\',\'camera_streedside\')"><img src="/main/img/loading.png" data-name="{i18n_Street}" data-src="/cameraStrasseImage" data-interval="3000"></div>';
-html += '<div><img src="/main/img/loading.png" data-name="{i18n_Automower}" data-src="/cameraAutomowerImage" data-interval="3000"></div>';
+//html += '<div style="cursor:pointer" onClick="mx.Actions.openEntryById(event,\'automation\',\'cameras\',\'camera_streedside\')"><img src="/main/img/loading.png" data-name="{i18n_Street}" data-src="/cameraStrasseImage" data-interval="3000"></div>';
+//html += '<div><img src="/main/img/loading.png" data-name="{i18n_Automower}" data-src="/cameraAutomowerImage" data-interval="3000"></div>';
+html += '</div>';
+html += '<div class="service imageWatcher">';
+html += '<div><img src="/main/img/loading.png" data-name="{i18n_Camera_01}" data-src="/camera01Image" data-interval="3000"></div>';
+html += '<div><img src="/main/img/loading.png" data-name="{i18n_Camera_02}" data-src="/camera02Image" data-interval="3000"></div>';
+html += '</div>';
+html += '<div class="service imageWatcher">';
+html += '<div><img src="/main/img/loading.png" data-name="{i18n_Camera_03}" data-src="/camera03Image" data-interval="3000"></div>';
+html += '<div><img src="/main/img/loading.png" data-name="{i18n_Camera_04}" data-src="/camera04Image" data-interval="3000"></div>';
+html += '</div>';
+html += '<div class="service imageWatcher">';
+html += '<div><img src="/main/img/loading.png" data-name="{i18n_Camera_07}" data-src="/camera07Image" data-interval="3000"></div>';
+html += '<div><img src="/main/img/loading.png" data-name="{i18n_Camera_09}" data-src="/camera09Image" data-interval="3000"></div>';
 html += '</div>';
 
 var cameraSubGroup = mx.Menu.getMainGroup('automation').addSubGroup('cameras', 900, '{i18n_Cameras}', 'proxy_camera.svg');
