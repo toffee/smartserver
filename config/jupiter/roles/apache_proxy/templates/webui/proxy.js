9b63252724512eccf1f9ef44c843d90525275173
Olivian Daniel Tofan <daniel@toffee.ro>
2021-03-07 08:32:46 +0200

    Add zigbee2mqtt, owntracks recorder and trial for flexisip

--- config/jupiter/roles/apache_proxy/templates/webui/proxy.js
+++ config/jupiter/roles/apache_proxy/templates/webui/proxy.js
@@ -3,8 +3,8 @@ var html = '<div class="service imageWatcher">';
 //html += '<div><img src="/main/img/loading.png" data-name="{i18n_Automower}" data-src="/cameraAutomowerImage" data-interval="3000"></div>';
 html += '</div>';
 html += '<div class="service imageWatcher">';
-html += '<div><img src="/main/img/loading.png" data-name="{i18n_Camera_01}" data-src="/camera01Image" data-interval="3000"></div>';
-html += '<div><img src="/main/img/loading.png" data-name="{i18n_Camera_02}" data-src="/camera02Image" data-interval="3000"></div>';
+html += '<div style="cursor:pointer" onClick="mx.Actions.openEntryById(event,\'automation\',\'cameras\',\'camera_01\')"><img src="/main/img/loading.png" data-name="{i18n_Camera_01}" data-src="/camera01Image" data-interval="3000"></div>';
+//html += '<div><img src="/main/img/loading.png" data-name="{i18n_Camera_02}" data-src="/camera02Image" data-interval="3000"></div>';
 html += '</div>';
 html += '<div class="service imageWatcher">';
 html += '<div><img src="/main/img/loading.png" data-name="{i18n_Camera_03}" data-src="/camera03Image" data-interval="3000"></div>';
@@ -17,7 +17,7 @@ html += '</div>';
 
 var cameraSubGroup = mx.Menu.getMainGroup('automation').addSubGroup('cameras', 900, '{i18n_Cameras}', 'proxy_camera.svg');
 cameraSubGroup.addHtml('cameras', html, function(){ mx.ImageWatcher.init('.service.imageWatcher > div'); }, 'user', 100 );
-cameraSubGroup.addUrl('camera_streedside','/gallery/?sub=camera');
+cameraSubGroup.addUrl('camera_01','/gallery/?sub=camera');
 
 var subGroup = mx.Menu.getMainGroup('other').getSubGroup('devices');
 subGroup.addUrl('automover', '/automowerDevice/', 'admin', 100, '{i18n_Automower}', '{i18n_Robonect}', false);
