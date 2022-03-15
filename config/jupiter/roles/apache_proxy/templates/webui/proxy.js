a50e2e4464c718e5a00777a7f8e9eab7d902cf90
Olivian Daniel Tofan <daniel@toffee.ro>
2022-03-15 20:51:06 +0200

    add links to router and switch pages update unifi and zigbee2mqtt

--- config/jupiter/roles/apache_proxy/templates/webui/proxy.js
+++ config/jupiter/roles/apache_proxy/templates/webui/proxy.js
@@ -2,20 +2,11 @@ var html = '<div class="service imageWatcher">';
 //html += '<div style="cursor:pointer" onClick="mx.Actions.openEntryById(event,\'automation\',\'cameras\',\'camera_streedside\')"><img src="/main/img/loading.png" data-name="{i18n_Street}" data-src="/cameraStrasseImage" data-interval="3000"></div>';
 //html += '<div><img src="/main/img/loading.png" data-name="{i18n_Automower}" data-src="/cameraAutomowerImage" data-interval="3000"></div>';
 html += '</div>';
-html += '<div class="service imageWatcher">';
-html += '<div style="cursor:pointer" onClick="mx.Actions.openEntryById(event,\'automation\',\'cameras\',\'camera_01\')"><img src="/main/img/loading.png" data-name="{i18n_Camera_01}" data-src="/camera01Image" data-interval="3000"></div>';
-//html += '<div><img src="/main/img/loading.png" data-name="{i18n_Camera_02}" data-src="/camera02Image" data-interval="3000"></div>';
-html += '</div>';
-html += '<div class="service imageWatcher">';
-html += '<div><img src="/main/img/loading.png" data-name="{i18n_Camera_03}" data-src="/camera03Image" data-interval="3000"></div>';
-html += '<div><img src="/main/img/loading.png" data-name="{i18n_Camera_04}" data-src="/camera04Image" data-interval="3000"></div>';
-html += '</div>';
-html += '<div class="service imageWatcher">';
-html += '<div><img src="/main/img/loading.png" data-name="{i18n_Camera_07}" data-src="/camera07Image" data-interval="3000"></div>';
-html += '<div><img src="/main/img/loading.png" data-name="{i18n_Camera_09}" data-src="/camera09Image" data-interval="3000"></div>';
-html += '</div>';
 
 var cameraSubGroup = mx.Menu.getMainGroup('automation').addSubGroup('cameras', 900, '{i18n_Cameras}', 'proxy_camera.svg');
 //cameraSubGroup.addHtml('cameras', html, function(){ mx.ImageWatcher.init('.service.imageWatcher > div'); }, 'user', 100 );
 //cameraSubGroup.addUrl('camera_01','/gallery/?sub=camera');
 
+var subGroup = mx.Menu.getMainGroup('admin').getSubGroup('devices');
+subGroup.addUrl('router', 'http://{{router_ip}}', 'admin', 200, '{i18n_Router}', '{i18n_Router (Extern)}', true, 'huawei.svg');
+subGroup.addUrl('switch', 'https://{{switch_ip}}', 'admin', 300, '{i18n_Switch}', '{i18n_Switch}', true);
