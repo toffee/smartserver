3e794fe3ccd6becb5a79d90b6a556dc4b7652657
Olivian Daniel Tofan <daniel@toffee.ro>
2023-07-30 13:29:01 +0300

    integrate unifi (svg still do not work)

--- config/jupiter/roles/unifi/templates/webui/unifi.js
+++ config/jupiter/roles/unifi/templates/webui/unifi.js
@@ -1,1 +1,2 @@
-mx.Menu.getMainGroup('admin').getSubGroup('tools').addUrl('unifi', '//unifi.{host}/','admin', 350, '{i18n_Unifi}', '{i18n_Unifi}', 'unifi_logo.svg', false);
+mx.Menu.getMainGroup('admin').getSubGroup('tools').addUrl('unifi', '//unifi.{host}/','admin', 350, '{i18n_Unifi}', '{i18n_Unifi}', 'unifi_logo.svg', true);
+                                          
