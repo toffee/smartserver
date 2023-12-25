387fb5f01aa48c41fe9ca8f6afd87755c8697297
Olivian Daniel Tofan <daniel@toffee.ro>
2023-12-25 19:58:42 +0200

    implement FireFly III on local server

---
+++ config/jupiter/roles/fireflyiii/templates/webui/fireflyiii.js
@@ -0,0 +1,4 @@
+var subGroup = mx.Menu.getMainGroup('workspace').addSubGroup('financial', 180, '{i18n_Financial}','financial_logo.svg');
+subGroup.addUrl('fireflyiii', '//fireflyiii.{host}/', ['user'], 181, '{i18n_FireflyIII}', '{i18n_FireflyIII}', "fireflyiii_logo.svg", true);
+subGroup.addUrl('fireflyiii_importer', '/fireflyiii-importer/', ['user'], 182, '{i18n_FireflyIII Importer}', '{i18n_FireflyIII Importer}', "fireflyiii_logo.svg", true);
+
