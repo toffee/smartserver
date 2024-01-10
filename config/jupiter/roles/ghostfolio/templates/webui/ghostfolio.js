423ed2eb0d6b0ea61b4a99bb7f62e1981f233713
Olivian Daniel Tofan <daniel@toffee.ro>
2024-01-10 22:28:31 +0200

    add ghostfolio role

---
+++ config/jupiter/roles/ghostfolio/templates/webui/ghostfolio.js
@@ -0,0 +1,3 @@
+var subGroup = mx.Menu.getMainGroup('workspace').getSubGroup('financial');
+subGroup.addUrl('ghostfolio', '//ghostfolio.{host}/', ['user'], 183, '{i18n_Ghostfolio}', '{i18n_Ghostfolio}', "ghostfolio_logo.svg", true);
+
