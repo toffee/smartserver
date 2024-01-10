c99807c1b7f85e4340dccaccdc8c349e9313c5db
Olivian Daniel Tofan <daniel@toffee.ro>
2024-01-10 22:29:47 +0200

    update from upstream

---
+++ config/jupiter/roles/menuentries/templates/webui/proxy.js
@@ -0,0 +1,4 @@
+var subGroup = mx.Menu.getMainGroup('admin').getSubGroup('devices');
+subGroup.addUrl('gateway', 'https://{{default_server_gateway}}', 'admin', 220, '{i18n_Router}', '{i18n_FritzBox}', "device_wifi.svg", true);
+subGroup.addUrl('router', 'http://{{router_ip}}', 'admin', 230, '{i18n_Router}', '{i18n_Router (Extern)}', 'router.svg', true);
+subGroup.addUrl('switch', 'https://{{switch_ip}}', 'admin', 241, '{i18n_Switch}', '{i18n_Switch}', 'switch.svg', true);
