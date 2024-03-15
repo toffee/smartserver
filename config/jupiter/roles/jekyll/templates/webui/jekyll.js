b75a75412d4bce340757a752e7421c76ea57cfce
Olivian Daniel Tofan <daniel@toffee.ro>
2024-03-15 14:58:46 +0200

    implement jekyll, vdirsyncer

---
+++ config/jupiter/roles/jekyll/templates/webui/jekyll.js
@@ -0,0 +1,2 @@
+var subGroup = mx.Menu.getMainGroup('workspace').getSubGroup('financial');
+subGroup.addUrl('jelyll', '//jekyll.{host}/', ['user'], 183, '{i18n_Jekyll}', '{i18n_Jekyll}', "jekyll_logo.svg", true);
