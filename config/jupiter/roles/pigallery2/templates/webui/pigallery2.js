19a70313fcd05dc4b9553d6d672e71f51162720e
Olivian Daniel Tofan <daniel@toffee.ro>
2024-11-17 16:44:30 +0200

    add pigallery2 role (initial commit)

---
+++ config/jupiter/roles/pigallery2/templates/webui/pigallery2.js
@@ -0,0 +1,3 @@
+var subGroup = mx.Menu.getMainGroup('workspace').getSubGroup('financial'); 
+subGroup.addUrl('pigallery2', ['user'], '/gallery/', { 'order': 183, 'title': '{i18n_Gallery}', 'info': '{i18n_Gallery}', 'icon': "pigallery2_logo.svg", 'target': '_blank'});
+
