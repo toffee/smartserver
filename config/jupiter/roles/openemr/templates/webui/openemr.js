1b355b592cd3d60de058a1a2cf8e59c799fc5d46
Olivian Daniel Tofan <daniel@toffee.ro>
2026-03-03 23:14:12 +0200

    add openemr directory

---
+++ config/jupiter/roles/openemr/templates/webui/openemr.js
@@ -0,0 +1,3 @@
+var subGroup = mx.Menu.getMainGroup('workspace').getSubGroup('financial');
+subGroup.addUrl('openemr', ['user'], '//openemr.{host}/', { 'order': 190, 'title': '{i18n_OpenEMR}', 'info': '{i18n_OpenEMR}', 'icon': "openemr_logo.svg", 'target': '_blank'});
+
