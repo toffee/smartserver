d9e710c3d88014165e3ddf90f36cb7af38cbfdf1
Olivian Daniel Tofan <daniel@toffee.ro>
2026-07-02 00:05:10 +0300

    fix url redirect to local host and software updates

--- config/jupiter/roles/fireflyiii/templates/webui/fireflyiii.js
+++ config/jupiter/roles/fireflyiii/templates/webui/fireflyiii.js
@@ -1,4 +1,3 @@
 var subGroup = mx.Menu.getMainGroup('workspace').addSubGroup('financial', { 'order': 180, 'title': '{i18n_Financial}', 'icon': 'financial_logo.svg' });
 subGroup.addUrl('fireflyiii', ['user'], '//fireflyiii.{host}/', { 'order': 181, 'title': '{i18n_FireflyIII}', 'info': '{i18n_FireflyIII}', 'icon': "fireflyiii_logo.svg", 'target': '_blank'});
-subGroup.addUrl('fireflyiii_importer', ['user'], '/fireflyiii-importer/', { 'order': 182, 'title': '{i18n_FireflyIII Importer}', 'info': '{i18n_FireflyIII Importer}', 'icon': "fireflyiii_logo.svg", 'target': '_blank'});
-
+subGroup.addUrl('fireflyiii_importer_domain', ['user'], '//fireflyiii-importer.{host}/', { 'order': 182, 'title': '{i18n_FireflyIII Importer}', 'info': '{i18n_FireflyIII Importer}', 'icon': "fireflyiii_logo.svg", 'target': '_blank'});
