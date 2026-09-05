1223d7e4a0da090a9a36fca31878a18e408678d1
Olivian Daniel Tofan <daniel@toffee.ro>
2026-09-05 20:08:51 +0300

    add firefly iii importer for calminia

--- config/jupiter/roles/fireflyiii/templates/webui/fireflyiii.js
+++ config/jupiter/roles/fireflyiii/templates/webui/fireflyiii.js
@@ -1,3 +1,4 @@
 var subGroup = mx.Menu.getMainGroup('workspace').addSubGroup('financial', { 'order': 180, 'title': '{i18n_Financial}', 'icon': 'financial_logo.svg' });
 subGroup.addUrl('fireflyiii', ['user'], '//fireflyiii.{host}/', { 'order': 181, 'title': '{i18n_FireflyIII}', 'info': '{i18n_FireflyIII}', 'icon': "fireflyiii_logo.svg", 'target': '_blank'});
 subGroup.addUrl('fireflyiii_importer_domain', ['user'], '//fireflyiii-importer.{host}/', { 'order': 182, 'title': '{i18n_FireflyIII Importer}', 'info': '{i18n_FireflyIII Importer}', 'icon': "fireflyiii_logo.svg", 'target': '_blank'});
+subGroup.addUrl('fireflyiii_importer_calminia_domain', ['user'], '//fireflyiii-importer-calminia.{host}/', { 'order': 183, 'title': '{i18n_FireflyIII Importer Calminia}', 'info': '{i18n_FireflyIII Importer Calminia}', 'icon': "fireflyiii_logo.svg", 'target': '_blank'});
